d059e7a57c99a02df395eb245c4dd11aa5f11545
Talon-Wu <148297662+Talon-Wu@users.noreply.github.com>
2023-11-16 13:10:41 +0000

    Initialization

---
+++ src/Player.java
@@ -0,0 +1,37 @@
+public class Player implements PlayerInterface, Runnable{
+    @Override
+    public void run() {
+
+    }
+    @Override
+    public boolean checkWin() {
+        return false;
+    }
+
+    @Override
+    public boolean declareAWin() {
+        return false;
+    }
+
+    @Override
+    public boolean pickCard() {
+        return false;
+    }
+
+    @Override
+    public boolean discardCard() {
+        return false;
+    }
+
+    @Override
+    public boolean outputDeck() {
+        return false;
+    }
+
+    @Override
+    public boolean writePlayer() {
+        return false;
+    }
+
+
+}
---
+++ src/PlayerInterface.java
@@ -0,0 +1,10 @@
+public interface PlayerInterface {
+
+    public boolean checkWin();
+    public boolean declareAWin();
+    public boolean pickCard();
+    public boolean discardCard();
+    public boolean outputDeck();
+    public boolean writePlayer();
+
+}
